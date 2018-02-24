e74724184f727962921e971f967d6b8bdb4ca395
Chelsea Edwards <chels.noelani@gmail.com>
2018-02-24 03:13:03 +0000

    Updated Repository

--- app/views/beers/_beer.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! beer, :id, :release, :price, :description, :img_url, :created_at, :updated_at
-json.url beer_url(beer, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224021201) do
+ActiveRecord::Schema.define(version: 20180224024623) do
 
   create_table "beers", force: :cascade do |t|
     t.integer  "release"
